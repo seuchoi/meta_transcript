7038fb7a24ba2a8a2dac64249fa003c165d57ca7
Seung Hoan Choi <seuchoi@gmail.com>
2023-10-11 16:35:06 -0400

    final script

--- src/transcript_single_analysis_meta_nocov.R
+++ src/transcript_single_analysis_meta_nocov.R
@@ -1,10 +1,10 @@
 ###
 ###
 ### perfrom meta-analysis of
-study_path<-c("test1.RData","test2.RData")
+#study_path<-c("test1.RData","test2.RData")
 transcript_single_analysis_meta_nocovout<-function(study_path=study_path,test=c("Burden"),
                                    min_study_cmac=1,min_meta_cmac=20,
-                                   use.anytranscript=T,combine.pval=T){ #(0)#
+                                   use.anytranscript=T){ #(0)#
 
 ### output filename
 ### grouping filename: transcript_id column is required
@@ -51,7 +51,7 @@ transcript_single_analysis_meta_nocovout<-function(study_path=study_path,test=c(
 ## start gene level
    for(group in groupings){#(1)#
     outnums <- c(1,seq(0,n__groupings,by=10),n__groupings)    # ???????
-    print(num)
+    #print(num)
     if(num %in% outnums){
        cat('Busy with group', group, 'which is', num, 'out of', n__groupings, '...\n')
     }
@@ -64,11 +64,11 @@ transcript_single_analysis_meta_nocovout<-function(study_path=study_path,test=c(
 
     if(length(transcripts)>0){ #(2)#
       for(tt in 1:length(transcripts)){ #(3)#
-         print(tt)
+         #print(tt)
          transID <- transcripts[tt]
-         n_studies_effective <- NA
+         n_studies_effective <- NULL
          variant.id <- n.alts <- NULL
-         n.site.list<-n.alts.list<-V.list <- U.list <- NULL
+         n.site.list<-n.alt.list<-V.list <- U.list <- NULL
 
       # --------------------------------------------------------------------
       # creating sv.list and V.list
@@ -76,223 +76,131 @@ transcript_single_analysis_meta_nocovout<-function(study_path=study_path,test=c(
 
           trans_results <- subset(study_list[[i]]$by.transcript,Transcript==transID & n.alt>min_study_cmac)
           if(nrow(trans_results)>0){
+          n_studies_effective<-c(n_studies_effective,1)
           U.list<-c(U.list,trans_results$Burden.Score)
           V.list<-c(V.list,trans_results$Burden.Variance)
           n.site.list<-c(n.site.list,trans_results$n.site)
-          n.alts.list<-c(n.site.list,trans_results$n.alts)
+          n.alt.list<-c(n.alt.list,trans_results$n.alt)
         }else{
+          n_studies_effective<-c(n_studies_effective,0)
           U.list<-c(U.list,NA)
           V.list<-c(V.list,NA)
           n.site.list<-c(n.site.list,NA)
-          n.alts.list<-c(n.site.list,NA)
+          n.alt.list<-c(n.alt.list,NA)
           }
         }
         U.sum<-sum(U.list,na.rm=T)
         V.sum<-sum(V.list,na.rm=T)
 
-          if(nrow(trans_group_variants)>0){
-          # Generate ChrPosRefAlt from Annotation
-            trans_group_variants$variant.id<-paste(trans_group_variants$chr,trans_group_variants$pos,
-                                                   trans_group_variants$ref,trans_group_variants$alt,sep=":")
+        out <- data.frame(group, transcript=transID, n_studies_effective=sum(n_studies_effective),cMAC=sum(n.alt.list), stringsAsFactors=F)
+        colnames(out) <- c("Group", "Transcript","n.studies.contributing", "cMAC")
+        nsites<-data.frame(t(n.site.list))
+        colnames(nsites)<-paste0("n.sites.",1:length(n.site.list))
+        out<-cbind(out,nsites)
+        nalt<-data.frame(t(n.alt.list))
+        colnames(nalt)<-paste0("n.alts.",1:length(n.alt.list))
+        out<-cbind(out,nalt)
+        rownames(out) <- group
 
-          # info from model results
-            total_variant <- study_list[[i]]$variantInfo[[group]]
-            total_cov     <- study_list[[i]]$covariance_matrix[[group]]
+          if("Burden" %in% test){
+            out0<-burden.fun(U.sum=U.sum,V.sum=V.sum)
+           out[,names(out0)]<-unlist(out0)
+         }
+         out$Est.SE<-1/sqrt(out$Burden.Variance)
+         out$Est <- out$Burden.Score/out$Burden.Variance
 
-          # overlap between results and annotation
-            sub_score <- total_variant[total_variant$variant.id %in% trans_group_variants$variant.id,]
-            sub_score <- unique(sub_score)
-
-          # update variant.id, n.alts, sv.list, V.list only if sum(MAC) >= threshold
-            if(sum(sub_score$MAC)>=min_study_cmac){
-              variant.id <- c(variant.id, sub_score$variant.id)
-              n.alts     <- c(n.alts, sum(sub_score$MAC))
-
-              sub_score    <- sub_score[order(sub_score$variant.id),]
-              sv.list[[i]] <- sub_score
-              vindex       <- sub_score$variant.id
-              V.list[[i]]  <- total_cov[vindex,vindex]
-            }
-          }
-          # creating summary file "out"  ??????????????????????????????????????????????????
-          out <- data.frame(group, transcript=transID, n_studies_effective=length(sv.list),
-                            n.site=length(unique(variant.id)), n.alt=sum(n.alts), stringsAsFactors=F)
-          colnames(out) <- c("Group", "Transcript","n.studies.contributing", "n.site", "n.alt")
-          rownames(out) <- group
-          class(out$n.studies.contributing) <- class(out$n.site) <- class(out$n.alt) <- "integer"
-        } #(4)#
-
-      # Fix when one sv list is 0
-        if(length(sv.list)>0){
-           effective_studies   <- which(!unlist(lapply(sv.list,is.null)))
-           n_studies_effective <- length(effective_studies)
-        }else{
-           effective_studies   <- NA
-           n_studies_effective <- 0
-        }
-      # --------------------------------------------------------------------
-
-      # --------------------------------------------------------------------
-        if(n_studies_effective>0){ #(5)#
-         # check
-           for(i in c(effective_studies)){
-              check  <- nrow(sv.list[[i]])
-              check1 <- sv.list[[i]]$variant.id == colnames(V.list[[i]])
-              check2 <- colnames(V.list[[i]]) == rownames(V.list[[i]])
-              if(F %in% c(check1, check2)){
-                stop("Warning: for cohort ", i,
-                     "the variants in the single var file, colnames of Covariance file, or rownames of covariance file do not match.\n")
-              }
-           }
-
-         # Prepare
-           variant.list <- lapply(sv.list, function(x) x$variant.id)
-           variant.list <- unique(do.call(c, variant.list))
-           variant.list <- variant.list[order(variant.list)]
-
-           n.site <- n.variants <- length(variant.list)
-           out$n.site <- n.site
-           print(length(variant.list))
-
-         # Reconstruct variant and covariance matrix
-           U <- matrix(0, n.variants, 1, dimnames=list(variant.list, 'Score'))
-           V <- matrix(0, n.variants, n.variants, dimnames=list(variant.list, variant.list))
-
-           for(i in c(effective_studies)){
-             variant.list.cohort     <- sv.list[[i]]$variant.id
-             variant.list.cohort     <- variant.list.cohort[order(variant.list.cohort)]
-             sv.list[[i]]            <- sv.list[[i]][order(sv.list[[i]]$variant.id),]
-             U[variant.list.cohort,] <- U[variant.list.cohort,] + sv.list[[i]]$Score
-             if(is.null(ncol(V.list[[i]]))){
-               V[variant.list.cohort, variant.list.cohort] <- matrix(V[variant.list.cohort, variant.list.cohort] + V.list[[i]])
-             }else{
-               V[variant.list.cohort, variant.list.cohort] <- matrix(V[variant.list.cohort, variant.list.cohort] + V.list[[i]][variant.list.cohort,variant.list.cohort])
-             }
-           }
-
-         #############################
-         ###        Run test       ###
-         #############################
-         # Start with Burden, this is also used for SMMAT test, and also for SKAT when implemented later
-           U.sum <- sum(U[,'Score'])
-           V.sum <- sum(V)
-#           GG1   <- rowSums(V)
-
-         # Then using adapted GENESIS script
-         ### Burden test
-
-            if("Burden" %in% test){
-              out0<-burden.fun(U.sum=U.sum,V.sum=V.sum)
-             out[,names(out0)]<-unlist(out0)
-           }
-         #############################
-
-        } #(5)#
         res <- dplyr::bind_rows(res, out)
-
-      } #(3)#
-    } #(2)#
-
-   } #(1)#
-
-## combine p-values using cauchy distribution
-   if(combine.pval){
-      res<-comb_meta_pvalue(data=res,min.cmac=min_meta_cmac)
-   }
-
-return(res)
+      }
+    }
+  }
+  res<-comb_meta_burden_pvalue(data=res,min.cmac=min_meta_cmac)
+  return(res)
 } #(0)#
 
 
 ####
 #### combined p-value from meta-analysis
 
-comb_meta_pvalue<-function(data,pval.col=c("Burden.pval","SKAT.pval","SKATO.pval","SMMAT.pval"),min.cmac=10){
+comb_meta_burden_pvalue<-function(data,pval.col=c("Burden.pval"),min.cmac=10){
 
 result<-list()
 pvalues<-names(data)[names(data) %in% pval.col]
 
-data<-subset(data,n.alt>=min.cmac)
-data$Cauchy.pval<-apply(data[,pvalues],1,function(x){x<-na.omit(x);CCT(x)})
+data<-subset(data,cMAC>=min.cmac)
+#data$Cauchy.pval<-apply(data[,pvalues],1,function(x){x<-na.omit(x);CCT(x)})
 
 genenames<-unique(data$Group)
 
 sum1<-NULL
 for (gg in 1:length(genenames)){
 genename<-genenames[gg]
-gdata<-subset(data,Group==genename &n.alt>=min.cmac)
+gdata<-subset(data,Group==genename &cMAC>=min.cmac)
 
 if(nrow(gdata)>0){
 gpvalue<-unique(unlist(gdata[,pvalues]))
 gpvalue<-na.omit(gpvalue)
 total.cauchy.pval<-CCT(gpvalue)
-sum0<-data.frame(Group=genename,Cauchy.anytranscript.pval=total.cauchy.pval)
-sum1<-rbind(sum1,sum0)
+gdata$Cauchy.anytranscript.pval<-total.cauchy.pval
+sum1<-rbind(sum1,gdata)
 }
 }
 
-gres0<-subset(data,Transcript %in% sum1$Group)
-gres1<-merge(gres0,sum1,by="Group")
-
-result[["by.transcript"]]<-data
-result[["combined.anytranscript"]]<-gres1
-
-return(result)
+return(sum1)
 }
 
 
 ###
 ###
 CCT <- function(pvals, weights=NULL){
-  #### check if there is NA
-  if(sum(is.na(pvals)) > 0){
-    stop("Cannot have NAs in the p-values!")
-  }
+#### check if there is NA
+if(sum(is.na(pvals)) > 0){
+stop("Cannot have NAs in the p-values!")
+}
 
-  #### check if all p-values are between 0 and 1
-  if((sum(pvals<0) + sum(pvals>1)) > 0){
-    stop("All p-values must be between 0 and 1!")
-  }
+#### check if all p-values are between 0 and 1
+if((sum(pvals<0) + sum(pvals>1)) > 0){
+stop("All p-values must be between 0 and 1!")
+}
 
-  #### check if there are p-values that are either exactly 0 or 1.
-  is.zero <- (sum(pvals==0)>=1)
-  is.one <- (sum(pvals==1)>=1)
-  if(is.zero && is.one){
-    stop("Cannot have both 0 and 1 p-values!")
-  }
-  if(is.zero){
-    return(0)
-  }
-  if(is.one){
-    warning("There are p-values that are exactly 1!")
-    return(1)
-  }
+#### check if there are p-values that are either exactly 0 or 1.
+is.zero <- (sum(pvals==0)>=1)
+is.one <- (sum(pvals==1)>=1)
+if(is.zero && is.one){
+stop("Cannot have both 0 and 1 p-values!")
+}
+if(is.zero){
+return(0)
+}
+if(is.one){
+warning("There are p-values that are exactly 1!")
+return(1)
+}
 
-  #### check the validity of weights (default: equal weights) and standardize them.
-  if(is.null(weights)){
-    weights <- rep(1/length(pvals),length(pvals))
-  }else if(length(weights)!=length(pvals)){
-    stop("The length of weights should be the same as that of the p-values!")
-  }else if(sum(weights < 0) > 0){
-    stop("All the weights must be positive!")
-  }else{
-    weights <- weights/sum(weights)
-  }
+#### check the validity of weights (default: equal weights) and standardize them.
+if(is.null(weights)){
+weights <- rep(1/length(pvals),length(pvals))
+}else if(length(weights)!=length(pvals)){
+stop("The length of weights should be the same as that of the p-values!")
+}else if(sum(weights < 0) > 0){
+stop("All the weights must be positive!")
+}else{
+weights <- weights/sum(weights)
+}
 
-  #### check if there are very small non-zero p-values
-  is.small <- (pvals < 1e-16)
-  if (sum(is.small) == 0){
-    cct.stat <- sum(weights*tan((0.5-pvals)*pi))
-  }else{
-    cct.stat <- sum((weights[is.small]/pvals[is.small])/pi)
-    cct.stat <- cct.stat + sum(weights[!is.small]*tan((0.5-pvals[!is.small])*pi))
-  }
+#### check if there are very small non-zero p-values
+is.small <- (pvals < 1e-16)
+if (sum(is.small) == 0){
+cct.stat <- sum(weights*tan((0.5-pvals)*pi))
+}else{
+cct.stat <- sum((weights[is.small]/pvals[is.small])/pi)
+cct.stat <- cct.stat + sum(weights[!is.small]*tan((0.5-pvals[!is.small])*pi))
+}
 
-  #### check if the test statistic is very large.
-  if(cct.stat > 1e+15){
-    pval <- (1/cct.stat)/pi
-  }else{
-    pval <- 1-pcauchy(cct.stat)
-  }
-  return(pval)
+#### check if the test statistic is very large.
+if(cct.stat > 1e+15){
+pval <- (1/cct.stat)/pi
+}else{
+pval <- 1-pcauchy(cct.stat)
+}
+return(pval)
 }
