09e933fe922451b61f4405dac9a2d421ac79c6e8
Seung Hoan Choi <seuchoi@gmail.com>
2024-02-22 13:24:52 -0500

    update characters

--- src/ukbb/kernel_variance_component_r4.2.2_ukbb.R
+++ src/ukbb/kernel_variance_component_r4.2.2_ukbb.R
@@ -10,14 +10,14 @@ nullfile=as.character(args[5])
 outfile=as.character(args[6])
 
 ## specify the packages
-.libPaths(c(“rpackages4_2_2”,.libPaths()))
+.libPaths(c("rpackages4_2_2",.libPaths()))
 
 ## load source scripts
-source(“UKBB_200KWES_CVD/GENESIS_adaptation_source.R”)
-source(“meta_transcript/src/ExtractKernalStatistics_SPA_transcript_UKBB.R”)
+source("UKBB_200KWES_CVD/GENESIS_adaptation_source.R")
+source("meta_transcript/src/ExtractKernalStatistics_SPA_transcript_UKBB.R")
 
 ## perfrom analysis
-kernell_variance_component_ukbb(gdsfile=gdsfile,groupfile=groupfile,phenfile=phenfile,ID_col=ID_col,nullfile=nullfile,outfile=outfile, test=“ExtractKernelStatistics”, vc.test=“Score.SPA”, AF.max=0.001, MAC.max=Inf,use.weights=FALSE)
+kernell_variance_component_ukbb(gdsfile=gdsfile,groupfile=groupfile,phenfile=phenfile,ID_col=ID_col,nullfile=nullfile,outfile=outfile, test="ExtractKernelStatistics", vc.test="Score.SPA", AF.max=0.001, MAC.max=Inf,use.weights=FALSE)
 
 ##quit!
 sessionInfo()
