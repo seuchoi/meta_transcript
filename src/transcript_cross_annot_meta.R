860961ffb6e2a9232e3da0742805e8027ccf871f
Seung Hoan Choi <seuchoi@gmail.com>
2024-07-31 23:03:57 -0400

    fix the error

--- src/transcript_cross_annot_meta.R
+++ src/transcript_cross_annot_meta.R
@@ -38,7 +38,7 @@ transcript_cross_annot_meta<-function(study_path=study_path,test=c("Burden"),
 ## start gene level
 ##   for(group in groupings[1:10]){#(1)#
     #for (gg in 1:length(groupings)){
-    for (gg in 1:587){
+    for (gg in 1:n__groupings){
     group<-groupings[gg]
     outnums <- c(1,seq(0,n__groupings,by=10),n__groupings)    # ???????
     #print(num)
